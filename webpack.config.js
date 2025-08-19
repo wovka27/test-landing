bba79c44e5966debe97f5011b96b5b0b19a0ffd6
Vladimir Demyanchuk <wovka.pb@yandex.ru>
2025-08-20 00:10:52 +0500

    [test-landing] refactor:

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   entry: './src/scripts/index.js',
   output: {
-    filename: 'js/main.js',
+    filename: 'js/[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     assetModuleFilename: 'assets/[hash][ext][query]',
     clean: true,
