3c03df2611f203721486d0b87eb2bacbc5b5ec35
Vladimir Demyanchuk <wovka.pb@yandex.ru>
2025-08-20 19:11:37 +0500

    [test-landing] refactor:

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,27 +7,21 @@ new Marquee({
   text: 'Дело помощи утопающим — дело рук самих утопающих! • Шахматы двигают вперёд не только культуру, но и экономику! • Лед тронулся, господа присяжные заседатели! • '
 })
 
-const carouselOptions = {
+new Carousel('[data-carousel-list]', {
   autoPlay: true,
   autoPlayDelay: 4000,
   pauseOnHover: true,
   infinite: true,
   transitionDuration: 600,
   slidesToShow: {767: 1, 960: 2, 1200: 3},
-  slidesToScroll: 1,
   prevButtonSelector: '[data-carousel-prev-btn]',
   nextButtonSelector: '[data-carousel-next-btn]',
   currentSlideSelector: '[data-carousel-current-slide]',
   totalSlidesSelector: '[data-carousel-total-slides]',
-}
-
-new Carousel('[data-carousel-list]', carouselOptions)
+})
 
 new Carousel('[data-carousel-list2]', {
-  ...carouselOptions,
   autoPlay: false,
-  autoPlayDelay: 4000,
-  pauseOnHover: false,
   activeBreakpoint: '(width <= 768px)',
   slidesToShow: {0: 1, 768: 1},
   infinite: false,
@@ -35,8 +29,6 @@ new Carousel('[data-carousel-list2]', {
   containerTrack: '.stages__track',
   prevButtonSelector: '[data-carousel-prev-btn2]',
   nextButtonSelector: '[data-carousel-next-btn2]',
-  currentSlideSelector: null,
   paginationSelector: '.stages__dots',
-  totalSlidesSelector: null,
   pagination: true,
 })
